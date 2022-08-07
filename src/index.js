678237e016c896f7e7e9278c7fe1c5c6819098d4
Antônio Oliveira <antonio.filho128@gmail.com>
2022-08-06 21:20:31 -0300

    Add firebase

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -22,3 +24,18 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJdhTS_GoWkM7yd8au1MuIex8ZQjPC8PI",
+  authDomain: "keep-clone-37fc6.firebaseapp.com",
+  projectId: "keep-clone-37fc6",
+  storageBucket: "keep-clone-37fc6.appspot.com",
+  messagingSenderId: "397420543041",
+  appId: "1:397420543041:web:639db6c64a42236c481ac0",
+  measurementId: "G-F546ZS7JN1"
+};
+
+const app = initializeApp(firebaseConfig);
+const perf = getPerformance(app);
+
+console.log(perf);
